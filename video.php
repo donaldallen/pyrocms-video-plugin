7235f045764060379d782e2afde99bbdda8ba5a1
Donald Allen <donaldallen@gmail.com>
2012-08-14 11:01:35 -0600

    First commit

---
+++ video.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Plugin_video extends Plugin
+{
+	function youtube()
+	{
+		$id			= ($this->attribute('id') ? $this->attribute('id') : 'oHg5SJYRHA0');
+		$width	= ($this->attribute('width') ? $this->attribute('width') : '100%');
+		$height	= ($this->attribute('height') ? $this->attribute('height') : '315');
+		
+		return '<iframe src="http://www.youtube.com/embed/'.$id.'" width="'.$width.'" height="'.$height.'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+	}
+	
+	function vimeo()
+	{
+		$id			= ($this->attribute('id') ? $this->attribute('id') : '42722025');
+		$width	= ($this->attribute('width') ? $this->attribute('width') : '100%');
+		$height	= ($this->attribute('height') ? $this->attribute('height') : '315');
+		
+		return '<iframe src="http://player.vimeo.com/video/'.$id.'" width="'.$width.'" height="'.$height.'" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>';
+	}
+}
+
+/* End of file Plugin_video.php */
